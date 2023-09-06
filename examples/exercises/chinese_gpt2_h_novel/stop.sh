0dd1d5253384d893fa14bd23602a52cbab7c75e3
晴耕雨读 <qgyd2021@gmail.com>
2023-09-06 11:09:55 +0800

    [update]add chinese_gpt2_h_novel

--- examples/exercises/chinese_gpt2_h_novel/stop.sh
+++ examples/exercises/chinese_gpt2_h_novel/stop.sh
@@ -2,4 +2,4 @@
 
 kill -9 `ps -aef | grep 'run.sh' | grep -v grep | awk '{print $2}' | sed 's/\n/ /'`
 
-kill -9 `ps -aef | grep 'Transformers/bin/python3' | grep -v grep | awk '{print $2}' | sed 's/\n/ /'`
+kill -9 `ps -aef | grep 'PyTorch/bin/python3' | grep -v grep | awk '{print $2}' | sed 's/\n/ /'`
